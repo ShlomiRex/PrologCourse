5df915274996b80b47f79eee11eaa5e1fd115a6f
Shlomi Domnenko <vgtvgy1@gmail.com>
2021-07-15 16:17:01 +0300

    Fixed plus function

--- Mamans/Maman 1/Question 3/main.pl
+++ Mamans/Maman 1/Question 3/main.pl
@@ -13,20 +13,15 @@ dec(s(X), s(Z)):-
 
 
 plus(s(X), s(Y), s(Z)):-
-	plus(X, s(Y), Z); % Option 1: decrement X
-	plus(s(X), Y, Z). % Option 2: decrement Y
+	plus(X, s(Y), Z). % Option 1: decrement X
 plus(0, 0, 0).
 plus(0, Z, Z).
-plus(Z, 0, Z).
-	
-
+plus(X, 0, X).
 
 % plus(s(s(0)), s(s(s(0))), Z). % Z = s(s(s(s(s(0))))) 
 % plus(s(s(0)), Y, s(s(s(s(s(0)))))). % Y = s(s(s(0))) 
 % plus(X, s(s(s(0))), s(s(s(s(s(0)))))). % X = s(s(0)) 
 
 
-% s(0) % 1
-% s(s(0)) % 2
-% s(s(s(0))) % 3
+
 
