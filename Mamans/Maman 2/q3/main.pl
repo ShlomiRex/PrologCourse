c7bc74dac57ab6ec9211c5ee9bb10469b351aab9
Shlomi <vgtvgy1@gmail.com>
2021-07-30 22:08:57 +0300

    Question 3 works

--- Mamans/Maman 2/q3/main.pl
+++ Mamans/Maman 2/q3/main.pl
@@ -1,60 +1,32 @@
-house(green_house).
-house(red_house).
-house(white_house).
-house(yellow_house).
-house(blue_house).
-
-person(english).
-person(spanish).
-person(ukrain).
-person(norwegian).
-person(japan).
-
-street(A, B, C, D, E):-
-	next_to(A, B),
-	next_to(B, C),
-	next_to(C, D),
-	next_to(D, E).
+% houses(List):-
+% 	member([_, redHouse, english, _, _, _], List),
+% 	member([_, _, spanish, _, _, dog], List),
+% 	member([_, greenHouse, _, coffee, _, _], List),
+% 	member([_, _, ukrain, tea, _, _], List),
+% 	member([WhiteHouseNum, whiteHouse, _, _, _, _], List), member([GreenHouseNum, greenHouse, _, _, _, _], List), (Diff1 is WhiteHouseNum-GreenHouseNum, (Diff1 == 1 ; Diff1 == (-1))),
+% 	member([_, _, ukrain, tea, _, _], List),
+% 	member([_, _, ukrain, tea, _, _], List),
 
 
 
-% 1
-live_in_house(english, red_house).
-live_in_house(X, green_house):-
-	X \= english.
+houses(List):-
+	%[NUMBER, COLOR, ORIGIN, DRINK, SMOKE, PET]
+    List = [[1, _, norway, _, _, _], [2, blueHouse, _, _, _, _], [3, _, _, milk, _, _], [4, _, _, _, _, _], [5, _, _, _, _, _]],  
+    member([_, redHouse, english, _, _, _], List),
+    member([_, _, spanish, _, _, dog], List),
+    member([_, greenHouse, _, coffee, _, _], List),
+    member([_, _, ukraine, tea, _, _], List),
+    member([WhiteHouseNum, whiteHouse, _, _, _, _], List), member([GreenHouseNum, greenHouse, _, _, _, _], List), (Diff1 is WhiteHouseNum-GreenHouseNum, (Diff1==1;Diff1==(-1))),
+    member([_, _, _, _, mrlboro, cat], List),
+    member([YellowHouseNum, yellow, _, _, time, _], List),
+    member([FoxPetHouseNum, _, _, _, _, fox], List), member([SmokesMontanaHouseNum, _, _, _, montana, _], List), (Diff2 is FoxPetHouseNum-SmokesMontanaHouseNum, (Diff2 == 1;Diff2==(-1))),
+    member([HorsePetHouseNum, _, _, _, _, horse], List), (Diff3 is YellowHouseNum-HorsePetHouseNum, (Diff3==1;Diff3==(-1))),
+    member([_, _, _, orange, luckystrike, _], List),
+    member([_, _, japan, _, parliament, _], List),
+    member([_, _, _, _, _, zebra], List),
+    member([_, _, _, water, _, _], List).
 
-% 2
-has_dog(spanish).
-
-% 3
-drinks_coffee(X):-
-	live_in_house(X, green_house
-).
-
-% 4
-drinks_tea(ukrain).
-
-% 5
-next_to(white_house, green_house).
-
-% 6
-smokes_marbalo(X):-
-	has_cat(X).
-
-% 7
-smokes_time(X):-
-	live_in_house(X, yellow_house).
-
-% 8
-drinks_milk(X):-
-	street(A, B, C, D, E),
-	live_in_house(X, C).
-
-% 9
-live_in_house(norwegian, HOUSE):-
-	street(HOUSE, B, C, D, E).
-
-next_to(X, Y):-
-	has_fox(Z),
-	live_in_house(Z, X),
-	
+answerQuestion(ZebraOwnerOrigin, WaterOwnerOrigin):-
+    houses(List),
+    member([_, _, ZebraOwnerOrigin, _, _, zebra], List),
+    member([_, _, WaterOwnerOrigin, water, _, _], List).
