ec5b75ebca1c1276a6861b09d13e5ad99023a02c
Shlomi <vgtvgy1@gmail.com>
2021-07-30 19:31:20 +0300

    save

--- Mamans/Maman 2/q2/main.pl
+++ Mamans/Maman 2/q2/main.pl
@@ -6,72 +6,38 @@ range(START, END, [START|Tail]):-
 
 % range( 2, 8, List). % List = [2, 3, 4, 5, 6, 7, 8] .
 
-
-
-
-% First argument is the list to divide.
-% Second argument is the first divided list.
-% Third argument is the second divided list.
-%divide([], [], []). % Extreme case.
-%divide([X], [X], []). % Extreme case.
-%divide([X, Y], [X], [Y]). % Extreme case.
-
-%%% Code 1 %%%
-% divide(L, L1, L2):-
-% 	div_head(L, L1, L2).
-
-% div_head([], [], []).
-% div_head([X], [X], []).
-% div_head([X, Y], [X], [Y]).
-% div_head([A|As], [A|L1], L2):-
-% 	div_tail(As, L1, L2).
-
-% div_tail([], [], []).
-% div_tail([X], [], [X]).
-% div_tail([X, Y], [X], [Y]).
-% div_tail([Bs|B], L1, [L2|B]):-
-% 	div_head(Bs, L1, L2).
-%%% Code 1 %%%
-%%% Code 2 %%%
-
-% divide([], [], []). % Extreme case.
-% divide([X], [X], []). % Extreme case.
-% divide([X, Y], [X], [Y]). % Extreme case.
-% divide(L, [X|Xs], [Ys|Y]):-
-% 	append(Xs, Ys, L).
-
-%%% Code 2 %%%
-
-%divide_first([A|As], [A|Xs], Ys):-
-%	divide_last(As, Xs, Ys).
-%divide_last([As|A], Xs, [Ys|A]):-
-%	divide_first(As, Xs, Ys).
-%
-%take_first_element([E|Es], List):-
-
-% divide([1, 2, 3, 4], List1, List2). % List1 = [1, 2], List2 = [3, 4].
-% divide([1, 2, 3, 4, 5, 6], List1, List2). % List1 = [1, 2, 3], List2 = [4, 5, 6].
-
-
-%%% Code 3 %%%
-
-% reverse([],Z,Z).
-
-% reverse([H|T],Z,Acc):-
-% 	reverse(T,Z,[H|Acc]).
-
-% divide([], [], []).
-% divide([X], [X], []).
-% divide([X, Y|Rest], [X|List1], [Y|List2]):-
-% 	reverse(Rest, ReversedRest, []),
-% 	reverse(ReversedRest, Z, []),
-% 	divide(Z, List1, List2).
-%%% Code 3 %%%
-
-
-
-
-div(L, A, B) :-
+divide(L, A, B):-
     append(A, B, L),
-    length(A, N),
-    length(B, N).
+    length(A, Len1),
+    length(B, Len2),
+	Len3 is Len1 - 1,
+	( Len1 == Len2 ; Len3 == Len2 ).
+
+% divide([1,2,3,4,5,6], List1, List2). % List1 = [1,2,3], List2 = [4,5,6] .
+% divide([1, 2, 3], List1, List2). % List1 = [1, 2], List2 = [3] .
+% range(1, 9, List), divide(List, A, B). % A = [1, 2, 3, 4, 5], B = [6, 7, 8, 9].
+
+shuffle([X, Y], _, [Y, X]).
+shuffle([], _, []).
+shuffle(_, 0, _).
+shuffle([X], _, [X]).
+
+%mix([X|FirstHalfRest], [Y|SecondHalfRest], [X, Y|Result]):-
+
+
+shuffle(List, Times, NewList):-
+	writeln(Times),
+	writeln(List),
+	NewTimes is Times - 1,
+	divide(List, FirstHalf, SecondHalf),
+	shuffle(FirstHalf, NewTimes, NewFirstHalf),
+	writeln("shuffling"),
+	shuffle(SecondHalf, NewTimes, NewSecondHalf),
+	writeln("new first half"),
+	writeln(NewFirstHalf),
+	writeln("new second half"),
+	writeln(NewSecondHalf),
+	append(NewFirstHalf, NewSecondHalf, NewList).
+
+% shuffle( [1,2,3,4,5,6,7,8,9,10], 4, List). 
+% range(1, 10, List), shuffle(List, 4, Result).
