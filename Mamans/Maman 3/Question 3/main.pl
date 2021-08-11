34935f4520c265947ac7ac254f835c2b9f110fac
Shlomi <vgtvgy1@gmail.com>
2021-08-11 17:46:13 +0300

    Maman 13 Question 3 works

--- Mamans/Maman 3/Question 3/main.pl
+++ Mamans/Maman 3/Question 3/main.pl
@@ -6,44 +6,42 @@ point(4,4).
 
 % Do not use findall, bagof and setof.
 
+
+%%%%%%%%%%%%%%%%%%%% NOTE: I used write and writeln for debugging, and I comment them out for final grading %%%%%%%%%%%%%%%%%%%%
+
 bestcompare(GT, X, Y):-
 	Term =.. [GT, X, Y],
-	write('Comparing: '), writeln(Term),
+	%write('Comparing: '), writeln(Term),
 	Term.
 
 
-findbest([X|Rest], GT, BestResult):-
-	writeln('findbest called'),
+findbest([X|Rest], GT):-
+	%writeln('findbest called'),
 	retract(bestval(Y)), % Get current best value
 	assert(bestval(Y)),
-	write('current best: '), writeln(Y),
-	write('Head of the list is: '), writeln(X),
+	%write('current best: '), writeln(Y),
+	%write('Head of the list is: '), writeln(X),
 	bestcompare(GT, X, Y), % If True, then we have a new best. 
-	!,
+	!, % Red cut - don't run the second 'findbest' because 100% that X is the new best.
 	retract(bestval(Y)),
 	assert(bestval(X)),
-	write('Setting BestResult to be '), writeln(X),
-	findbest(Rest, GT, X).
+	%write('Setting BestResult to be '), writeln(X),
+	findbest(Rest, GT).
 
-findbest([X|Rest], GT, BestResult):-
-	% X is better than BestResult, set BestResult as X
-	write(X), write(' is not better than BestResult: '), writeln(BestResult),
-	findbest(Rest, GT, BestResult).
-findbest([], _, BestResult):-
-	!,
-	retract(bestval(X)),
-	write('Finished finding best value: '), writeln(X),
-	BestResult = X,
-	writeln(BestResult).
+findbest([_|Rest], GT):-
+	% X is not better than best value so far.
+	%write(X), writeln(' is not better than current best value'),
+	findbest(Rest, GT).
+findbest([], _).
 
 
 bestof(Val, GT, Goal):-
 	getAllValues(Val, Goal, [Head|Values]), % Collect all values
-	write('asserting: '),
-	writeln(Head),
+	%write('asserting: '), writeln(Head),
 	assert(bestval(Head)), % Set BestResult to Head (first element)
-	findbest(Values, GT, BestVal), % Find best value
-	write('Best value is: '), writeln(BestVal),
+	findbest(Values, GT), % Find best value
+	retract(bestval(BestVal)),
+	%write('Best value is: '), writeln(BestVal),
 	Val is BestVal. % Return result
 
 
@@ -62,10 +60,21 @@ collect(L):-
 	;
 	L = [X|Rest], collect(Rest)).
 
+
+
+
 % bestof(Y,>,point(_,Y)). % Y = 10
 % bestof(Y,<,point(_,Y)). % Y = 2
 % bestof(X,>,point(X,_)). % X = 5
 % bestof(X,<,point(X,_)). % X = 1
 
-% findall(Y, point(_, Y), Res). % Res = [2, 10, 3, 2, 4].
+% Same as above but with one command (to clean memory):
+
+% [main], retractall(bestval(_)), bestof(Y,>,point(_,Y)). % Y = 10.
+% [main], retractall(bestval(_)), bestof(Y,<,point(_,Y)). % Y = 2.
+% [main], retractall(bestval(_)), bestof(X,>,point(X,_)). % X = 5.
+% [main], retractall(bestval(_)), bestof(X,<,point(X,_)). % X = 1
+
+
+
 
