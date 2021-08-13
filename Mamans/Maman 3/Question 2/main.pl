94b1eacaaee3b6d73264dffdf4b1eafeb51716a2
Shlomi <vgtvgy1@gmail.com>
2021-08-13 21:17:38 +0300

    Question 2 Maman 13 Basic print tic tac toe

--- Mamans/Maman 3/Question 2/main.pl
+++ Mamans/Maman 3/Question 2/main.pl
@@ -1,2 +1,37 @@
-begin(Winner):-
-	write('Welcome to TicTacToe! Made by Shlomi Domnenko!').
+% To run:
+% [main], retractall(board(_, _, _)), begin().
+
+
+/*
+---------------------
+		Board
+---------------------
+Consists of 3 rows.
+
+Empty cell: 0
+X Player: 1
+O Player: 2
+*/
+
+begin():-
+	writeln('Welcome to TicTacToe! Made by Shlomi Domnenko!'),
+	assert(board([0,0,0], [0,2,0], [0,1,0])),
+	printBoard().
+
+
+printBoard():-
+	retract(board(Row1, Row2, Row3)),
+	nl, nl,
+	printRow(Row1),
+	writeln('|---|---|---|'),
+	printRow(Row2),
+	writeln('|---|---|---|'),
+	printRow(Row3),
+	nl, nl,
+	assert(board(Row1, Row2, Row3)).
+printRow([Head|Tail]):-
+	write('|'),
+	((Head == 0,write('   ')) ; (Head == 1,write(' X ')) ; (Head == 2,write(' O '))),
+	printRow(Tail).
+printRow([]):-
+	writeln('|').
