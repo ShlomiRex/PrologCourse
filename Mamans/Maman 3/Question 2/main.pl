58e16b7e8aadd4b769fd5ab168c0522de00c7025
Shlomi <vgtvgy1@gmail.com>
2021-08-13 22:13:16 +0300

    save

--- Mamans/Maman 3/Question 2/main.pl
+++ Mamans/Maman 3/Question 2/main.pl
@@ -13,16 +13,43 @@ X Player: 1
 O Player: 2
 */
 
+/* Starting the game */
 begin():-
 	nl,nl,
 	writeln('Welcome to TicTacToe! Made by Shlomi Domnenko!'),
 	writeln('Starting player is X, second player is O'),
 	nl,
 	assert(board([0,0,0], [0,0,0], [0,0,0])),
+	playerTurn(1).
+
+
+
+
+playerTurn(Player):-
+	((Player == 1, writeln('Player X turn')) ; (writeln('Player O turn'))),
 	printBoard(),
 	writeln('Please select your cell'),
 	inputSelectCell(Row, Col).
 
+
+
+checkValidMove(Player, RowNumber, ColNumber):-
+	getCell(RowNumber, ColNumber, Cell).
+
+
+getRow(RowNumber, RowResult):-
+	retract(board(Row1, Row2, Row3)),
+	( (RowNumber == 1, RowResult is Row1) ; (RowNumber == 2, RowResult is Row2) ; (RowNumber == 3, RowResult is Row3) ),
+	assert(board(Row1, Row2, Row3)).
+getCell(RowNumber, ColNumber, CellResult):-
+	getRow(RowNumber, Row).
+
+
+getElementAtIndex([X|Xs], Index, Res):-
+	writeln(Index),
+	Index2 is Index - 1,
+	getElementAtIndex(Xs, Index2, Res), !.
+getElementAtIndex([X|_], 0, X).
 
 /* User Input - Start */
 inputSelectCell(Row, Col):-
@@ -33,14 +60,20 @@ inputSelectCell(Row, Col):-
 	(
 	integer(Row), integer(Col),
 	Row > 0, Row < 4,
-	Col > 0, Col < 4,
-	writeln('Success!')
+	Col > 0, Col < 4
 	) ; 
 	nl, writeln('Please enter valid numbers!'),
 	inputSelectCell(Row, Col).
 /* User Input - End */
 
+
+
+
+
+
+/* Print Board - Start */
 printBoard():-
+	%writeln('Printing board...'),
 	retract(board(Row1, Row2, Row3)),
 	nl, nl,
 	printRow(Row1),
@@ -56,3 +89,4 @@ printRow([Head|Tail]):-
 	printRow(Tail).
 printRow([]):-
 	writeln('|').
+/* Print Board - End */
