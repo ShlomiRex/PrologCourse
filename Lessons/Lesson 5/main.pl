622962b09352d88a1f6a444be3201cc5f7c212c1
Shlomi <vgtvgy1@gmail.com>
2021-08-25 19:46:26 +0300

    Update main.pl

--- Lessons/Lesson 5/main.pl
+++ Lessons/Lesson 5/main.pl
@@ -136,3 +136,240 @@ path_to_item(X,t(L,Y,R),[Y|Path]):-
 
 
 
+
+
+
+% Exam question worth 25 points
+% Find number of nodes, that the value of the node, is equal to number of nodes in the sub-tree.
+% But also, the number of access to a node is limited (O(1)).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This is the original code of the question
+% Because of countNodes, the leaf will be accessed this many times: height of the leaf.
+eq(nil,0).
+eq(t(L,X,R),N):-
+	eq(L,N1) , eq(R,N2), 
+	(countNodes(t(L,X,R),X) , ! ,  N is N1 + N2 + 1 
+	;
+	 N is N1 + N2 
+	).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% So professor proposed O(1) accesses per node like this:
+% i.e. save intermediate Size which is the number of nodes in the sub-tree.
+% Num is the number of nodes of the subtree which has Num nodes.
+ eq(Tree,Res):-
+    eq(Tree,_,Res).
+
+eq(nil,0,0).
+	 
+eq(t(L,X,R),Size,Num):-
+      eq(L,Size1,Num1),
+	  eq(R,Size2,Num2),
+	  Size is Size1+Size2+1,  
+	  (X=Size,!,Num is Num1+Num2+1
+	   ;
+	   Num is Num1+Num2).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+
+
+
+
+
+
+% Write a predict that returns the list of nodes, of the height X of the tree.
+% For example if X = 2 then return the list of nodes of height 2.
+
+% MY CODE - NOT WORKING
+/*
+listOfNodesOfHeight(nil,_,[]).
+listOfNodesOfHeight(T,H,Nodes):-
+	listOfNodesOfHeightHelper(T,0,H,Nodes).
+listOfNodesOfHeightHelper(nil,_,_,[]):-!.
+listOfNodesOfHeightHelper(t(L,X,R), CurrH, H, Nodes):-
+	CurrH \= H,
+	!,
+	NewCurrH is CurrH + 1,
+	listOfNodesOfHeightHelper(L, NewCurrH, H, Nodes1),
+	listOfNodesOfHeightHelper(R, NewCurrH, H, Nodes2),
+	append(Nodes1, Nodes2, Nodes), !.
+listOfNodesOfHeightHelper(t(_,X,_), CurrH, H, [X|_]):-
+	CurrH = H,
+	writeln(X),
+	!.
+*/
+
+% MY WORKING CODE
+listOfNodesOfHeight(t(L,_,R),H,Nodes):-
+	H > 0, 
+	!,
+	NewHeight is H - 1,
+	listOfNodesOfHeight(L,NewHeight,Nodes1),
+	listOfNodesOfHeight(R,NewHeight,Nodes2),
+	append(Nodes1,Nodes2,Nodes).
+listOfNodesOfHeight(t(_,X,_),0,[X]).
+listOfNodesOfHeight(nil,_,[]).
+
+% ?- T=t(t(nil,2,nil),4,t(t(nil,5,nil),6,t(nil,8,nil))), listOfNodesOfHeight(T, 0, Res). % Res = [4].
+% ?- T=t(t(nil,2,nil),4,t(t(nil,5,nil),6,t(nil,8,nil))), listOfNodesOfHeight(T, 1, Res). % Res = [2, 6].
+% ?- T=t(t(nil,2,nil),4,t(t(nil,5,nil),6,t(nil,8,nil))), listOfNodesOfHeight(T, 2, Res). % Res = [5, 8].
+
+
+
+
+
+
+
+
+
+
+
+
+% כתוב פרדיקט המקבל עץ ומחזיר את סכום הגבהים של צמתיו כולל של השורש
+% גובה של עץ מוגדר להיות כאורך המסלול המקסימלי מאותה צומת לעלה שמתחתיו
+sumHeight(t(L,_,R),Sum):-
+	maxHeight(t(L,_,R), H),
+	sumHeight(L,SumL),
+	sumHeight(R,SumR),
+	Sum is H + SumL + SumR.
+sumHeight(nil,0).
+% ?- T=t(t(nil,2,nil),4,t(t(nil,5,nil),6,t(nil,8,nil))), sumHeight(T, Res). % Res = 8.
+
+% Professor's Answer (more efficient):
+% sumHeight2(t(L,_,R),Sum,H):-
+% 	sumHeight2(L,SumL,H1),
+% 	sumHeight2(R,SumR,H2),
+% 	max(H1,H2,H3),
+% 	H is H3 + 1,
+% 	Sum is SumL + SumR + H.
+% sumHeight2(nil,0,-1).
+
+
+
+
+
+
+
+
+
+
+
+% Determin if the tree is binary search tree.
+ordered_tree(nil).
+ordered_tree(t(L,X,R)):-
+	bigger_than(X,L),
+	smaller_than(X,R),
+	ordered_tree(L),
+	ordered_tree(R).
+
+bigger_than(_,nil):-!.
+bigger_than(X,t(L,Y,R)):-
+	X > Y,
+	bigger_than(X,L),
+	bigger_than(X,R).
+
+smaller_than(_,nil):-!.
+smaller_than(X,t(L,Y,R)):-
+	X < Y,
+	smaller_than(X,L),
+	smaller_than(X,R).
+
+% ?- ordered_tree(t(t(nil,2,nil),4,t(t(nil,5,nil),6,t(nil,8,nil)))). % true.
+% ?- ordered_tree(t(t(nil,2,nil),4,t(t(nil,8,nil),6,t(nil,5,nil)))). % false.
+% ?- ordered_tree(t(nil, 5, nil)). % true.
+% ?- ordered_tree(t(t(nil, 6, nil), 5, nil)). % false.
+
+
+% More efficient solution
+
+ordered(Tree):-
+	ordered(Tree,-10000, 10000).
+% if needed the values of Min and Max can be determined by a pre-scan of the tree
+ordered(t(L,X,R), Min, Max):-
+	X>Min,
+	X<Max,
+	ordered(L,Min,X),
+	ordered(R,X,Max).
+ordered(nil,_,_).
+% ?- ordered(t(t(nil,2,nil),4,t(t(nil,5,nil),6,t(nil,8,nil)))). % true.
+% ?- ordered(t(t(nil,2,nil),4,t(t(nil,8,nil),6,t(nil,5,nil)))). % false.
+% ?- ordered(t(nil, 5, nil)). % true.
+% ?- ordered(t(t(nil, 6, nil), 5, nil)). % false.
+
+
+
+
+
+
+
+% More efficient answer (Notice we don't provide +10000 and -10000 as Min and Max - Prolog chooses for us)
+ordered2(Tree):-
+	ordered2(Tree, _, _).
+ordered2(t(L,X,R), Min, Max):-
+	(var(Min), ! ; X >= Min),
+	(var(Max), ! ; X < Max),
+	ordered2(L, Min, X),
+	ordered2(R, X, Max).
+ordered2(nil,_,_).
+% What is hapenning here?
+% At first, Min is variable. So prolog exits.
+% Bug prolog doesn't like when things return False. So it tries again. It SETS MIN AS A NUMBER IT CHOOSES.
+% So now Min is not Var. Then it checks if X >= Min. If not, then prolog exits and starts again, setting MIN to be DIFFIRENT number, untill it succeeds.
+% And so on, including Max.
+
+
+% ?- ordered2(t(t(nil,2,nil),4,t(t(nil,5,nil),6,t(nil,8,nil)))). % true.
+% ?- ordered2(t(t(nil,2,nil),4,t(t(nil,8,nil),6,t(nil,5,nil)))). % false.
+% ?- ordered2(t(nil, 5, nil)). % true.
+% ?- ordered2(t(t(nil, 6, nil), 5, nil)). % false.
+
+
+
+
+
+
+
+
+
+% Linked list with Tail so we can concatinate lists with O(1).
+% L=[1, 2, 3, 4, bob|Tail], Tail= [a,aaa,a,aa]
+
+% Concatinate two lists.
+% רשימות הפרש
+% Big O (1) - no recursion
+conc(L1-T1,L2-T2,Res - Tail):-
+    T1 = L2,
+    Res = L1,
+    Tail = T2.
+% ?- conc([1,2,343,bob|T1] - T1 , [a,b,c|T2]-T2 , Res - Tail).
+/* 
+T1 = [a, b, c|Tail],
+T2 = Tail,
+Res = [1, 2, 343, bob, a, b, c|Tail].
+*/
+
+
+
+
+
+
+
+
+
+
+
+% פרדיקט המקבל רשימה וממין אותה משתנים לתחילתה ואטומים או מספרים לסוף הרשימה
+
+var_atomic(L,Res):-
+		var_atomic(L,L1,L2),
+		conc(L1,L2,Res).
+
+var_atomic([],[],[]).
+
+var_atomic([X|Xs],[X|Rest1],L2):-
+	var(X),!,var_atomic(Xs,Rest1,L2).
+
+var_atomic([X|Xs],L1,[X|Rest2]):-
+	atomic(X),var_atomic(Xs,L1,Rest2).
